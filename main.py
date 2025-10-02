c5261edb26e8ab16b8c32fabb1b3d78f762ed461
xyndykate <martinorina24@gmail.com>
2025-10-02 22:41:37 +0300

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import requests
 
-response = requests.get('https://api.com/test-service')
+def get_stock_data():
+    url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&outputsize=full&apikey=demo"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        last_refreshed = data["Meta Data"]["3. Last Refreshed"]
+        price = data["Time Series (5min)"][last_refreshed]["1. open"]
+        return price
+    else:
+        return None
+
+price = get_stock_data()
+symbol = "IBM"
+if price is not None:
+    print(f"{symbol}: {price}")
+else:
+    print("Failed to retrieve data.")
